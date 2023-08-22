b1fb6257e55572e6eac7991da49f452ba68a27e0
LuckyMirik <Luckymirik@mail.ru>
2023-08-22 15:26:27 +0300

    Начал ДЗ

---
+++ src/test/java/pro/sky/employeewithtests/EmployeewithtestsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.employeewithtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeewithtestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
