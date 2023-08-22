54399873aaf7fc807f6d28714e7399579b36ebc1
LuckyMirik <Luckymirik@mail.ru>
2023-08-22 15:25:04 +0300

    Начал ДЗ

---
+++ src/main/java/pro/sky/employeewithtests/EmployeeWithTestsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.employeewithtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeWithTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeWithTestsApplication.class, args);
+	}
+
+}
